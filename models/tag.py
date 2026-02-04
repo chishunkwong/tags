3839394a6116934bd66ad9ccfdb60b8c4acaaf16
Chishun Kwong <chishunkwong@yahoo.com>
2026-02-04 11:51:02 -0500

    Better DB constraints

--- models/tag.py
+++ models/tag.py
@@ -1,4 +1,5 @@
 from .base import Base
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.schema import ForeignKey
 
@@ -6,6 +7,10 @@ class Tag(Base):
     __tablename__ = "tag"
     id: Mapped[int] = mapped_column(primary_key=True)
     tag_group_id: Mapped[int] = mapped_column(ForeignKey("tag_group.id"))
-    name: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str]
 
     tag_group = relationship("TagGroup", back_populates="tags")
+
+    __table_args__ = (
+        UniqueConstraint('tag_group_id', 'name', name='uix_tag_group_name'),
+    )
