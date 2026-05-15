072a5e7238145bbd06b7ee49619f2ed77743f888
Chishun Kwong <chishunkwong@yahoo.com>
2026-05-15 16:57:20 -0400

    To make the Up arrow responsive only on the media view page

--- static/helper.js
+++ static/helper.js
@@ -15,7 +15,7 @@ document.addEventListener('keydown', (event) => {
     else if ((keyValue === 'U' || keyValue === 'u') && window.undo_delete_url) {
       window.location.href = window.undo_delete_url;
     }
-    else if (keyValue === 'ArrowUp' || keyValue === 'L' || keyValue === 'l') {
+    else if ((keyValue === 'ArrowUp' || keyValue === 'L' || keyValue === 'l') && window.delete_url) {
       window.location.href = window.list_media;
     }
     else {
